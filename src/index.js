f04490befca4a59bded81c16cae3e5ed11d0d4e3
deckOfManyStrings <kingsadabobby@gmail.com>
2021-08-25 06:40:24 -0700

    Added list with react

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ ReactDOM.render(
   <div>
     <h1>Hello World</h1>
     <p>This is a paragraph</p>
+
+    <ul>
+      <li>Red</li>
+      <li>Yellow</li>
+      <li>Blue</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
